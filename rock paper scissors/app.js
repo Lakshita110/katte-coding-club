a7cfdaf34fd2e6d7e31ee0f57d73ef589ff19e6b
SNY6773 <90193867+SNY6773@users.noreply.github.com>
2021-09-20 20:08:14 +0200

    Finished XD // itfinally works//
    finally made it work even tho it is basic and ugly lol

--- rock paper scissors/app.js
+++ rock paper scissors/app.js
@@ -4,11 +4,13 @@ const YourChoiceDisplay = document.getElementById('Your-Choice')
 const possibleChoices = document.querySelectorAll('button')
 let YourChoice
 let ComputerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) =>{
     YourChoice=e.target.id 
     YourChoiceDisplay.innerHTML=YourChoice
-    generateComputerChoice()  
+    generateComputerChoice()
+    getresult()  
 }))
 
 function generateComputerChoice(){
@@ -24,4 +26,28 @@ function generateComputerChoice(){
         ComputerChoice = 'scissors'
     }
     ComputerChoiceDisplay.innerHTML=ComputerChoice
+}
+function getresult() {
+    if (ComputerChoice == YourChoice){
+      result='Draw'
+    }
+    if (ComputerChoice == 'rock'&&YourChoice== 'paper'){
+        result='You Won'
+    }
+    if (ComputerChoice =='rock'&&YourChoice=='scissors'){
+        result='You Lost'
+    }
+    if (ComputerChoice == 'paper'&&YourChoice== 'rock'){
+        result='You Lost'
+    }
+    if (ComputerChoice =='paper'&&YourChoice=='scissors'){
+        result='You Won'
+    }
+    if (ComputerChoice == 'scissors'&&YourChoice== 'rock'){
+        result='You Won'
+    }
+    if (ComputerChoice =='scissors' &&YourChoice=='paper'){
+        result='You Lost'
+    }
+    resultDisplay.innerHTML= result
 }
