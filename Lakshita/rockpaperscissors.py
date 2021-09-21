02a68989a35fce099b1d60d0f7a7ec864dc99edf
Lakshita Jain <44927035+Lakshita110@users.noreply.github.com>
2021-09-21 12:02:37 +0200

    Added guide for rock paper scissors game

---
+++ Lakshita/rockpaperscissors.py
@@ -0,0 +1,8 @@
+# Suggestions for steps needed for a rock paper scissors game
+
+""" 
+1. validate input from user 
+2. randomize computer's response 
+3. declare winner based on both responses
+
+"""
