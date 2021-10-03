27466f408f108f4d0ec19eb86599966dea859bee
Condemnedapostle <90193908+Condemnedapostle@users.noreply.github.com>
2021-10-03 21:03:16 +0200

    ...finally...probably....right?

--- Hiab/test.js
+++ Hiab/test.js
@@ -10,44 +10,44 @@ PossibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
     Player1 = e.target .id
     Player1Display.innerHTML=Player1
     generatePlayer2()
-    Retresult()
+    getResult()
 }))
 
 function generatePlayer2(){
     const randomNumber =Math.floor(Math.random() * 3) +1
     console.log(randomNumber)
     if(randomNumber=== 1){
-        Player2='rock'
+        Player2 = 'rock'
     }
     if(randomNumber=== 2){
-        Player2='paper'
+        Player2 = 'Paper'
     }
     if(randomNumber=== 3){
-        Player2='scissors'
+        Player2 = 'scissors'
     }
     Player2Display.innerHTML=Player2
 }
-function Retresult() {
-    if (Player2==Player1){
-        Result='Draw'
+function getResult() {
+    if (Player2 == Player1){
+        Result = 'Draw'
     }
-    if(Player2=='rock'&&Player1=='paper'){
-        Result='Player 1 Won'
+    if(Player2 == 'rock'&&Player1=='Paper'){
+        Result = 'Player1 Won'
     }
-    if(Player2=='paper'&&Player1=='scissors'){
-        Result='Player 1 Won'
+    if(Player2 == 'Paper'&&Player1=='scissors'){
+        Result = 'Player1 Won'
     }
-    if(Player2=='scissors'&&Player1=='rock'){
-        Result='Player 1 Won'
+    if(Player2 == 'scissors'&&Player1=='rock'){
+        Result = 'Player1 Won'
     }
-    if(Player2=='rock'&&Player1=='scissors'){
-        Result='Player 1 Lost'
+    if(Player2 == 'rock'&&Player1=='scissors'){
+        Result = 'Player1 Lost'
     }
     if(Player2=='paper'&&Player1=='rock'){
-        Result='Player 1 Lost'
+        Result='Player1 Lost'
     }
-    if(Player2=='scissors'&&Player1=='paper'){
-        Result='Player 1 Lost'
+    if(Player2=='scissors'&&Player1=='Paper'){
+        Result='Player1 Lost'
     }
     ResultDisplay.innerHTML=Result
 }
