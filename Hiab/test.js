997db8b7107e8ebcc978071184444ee7052a5552
Condemnedapostle <90193908+Condemnedapostle@users.noreply.github.com>
2021-10-03 20:00:04 +0200

    first form seems fine but..... more work needed

--- Hiab/test.js
+++ Hiab/test.js
@@ -1,1 +1,16 @@
-test
+const Player2Display= document.getElementById('Player-2')
+const Player1Display= document.getElementById('Player-1')
+const resultDisplay= document.getElementById('result')
+const PossibleChoices= document.querySelectorAll('button')
+let Player1
+let Player2
+let result
+
+PossibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) =>{
+    Player1 = e.target .id
+    Player1Display.innerHTML=Player1
+    generatePlayer2
+
+
+
+}))
